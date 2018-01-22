632a5e43961044deed233481d1333c130f65a716
Awlter <wxfwalter@gmail.com>
2018-01-22 23:15:56 +0800

    implement .search_by_title method for video model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Myflix::Application.routes.draw do
   root 'videos#index'
   get '/home', to: 'categories#index'
 
-  resources :videos, only: [:index, :show]
+  resources :videos, only: [:index, :show] do
+    collection do
+      get 'search'
+    end
+  end
+
   get 'ui(/:action)', controller: 'ui'
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Video, :focus do
+describe Video do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to belong_to(:category)}
@@ -8,5 +8,31 @@ describe Video, :focus do
   it 'is valid with title, description' do
     video = FactoryGirl.build(:video)
     expect(video.valid?).to be_truthy
+  end
+
+  describe ".search_by_title" do
+    let(:futurama) { FactoryGirl.create(:video, title: "Futurama") }
+    let(:futurama1) { FactoryGirl.create(:video, title: "Futurama1", created_at: 1.day.ago)}
+    before do
+      futurama
+    end
+
+    it "returns an empty array if there is no match" do
+      expect(Video.search_by_title('hello')).to eq([])
+    end
+
+    it "returns an empty array if title is an empry string" do
+      expect(Video.search_by_title('')).to eq([])
+    end
+    it "returns an array of one video with an exact match" do
+      expect(Video.search_by_title('Futurama')).to eq([futurama])
+    end
+    it "returns an array of one video with a partial match" do
+      expect(Video.search_by_title('tura')).to eq([futurama])
+    end
+    it "returns an array of matched videos ordered by created_at" do
+      futurama1
+      expect(Video.search_by_title('Futurama')).to eq([futurama1, futurama])
+    end
   end
 end
