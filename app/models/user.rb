455434b3b4fe36f640ae91afa4f9a944a552124a
Awlter <wxfwalter@gmail.com>
2018-01-27 20:03:26 +0800

    create review model and the association with user and video, and create macros for set_current_user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :full_name, :password
   validates_uniqueness_of :email
   validates :password, length: { minimum: 8}
+
+  has_many :reviews
   
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,6 @@ describe User do
   it { is_expected.to validate_presence_of :password }
   it { is_expected.to validate_uniqueness_of :email }
   it { is_expected.to ensure_length_of(:password).is_at_least(8) }
+
+  it { is_expected.to have_many(:reviews) }
 end
