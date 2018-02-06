b5c2d5b141e77f202859cd4e7ef16efe5fa0cf04
Awlter <wxfwalter@gmail.com>
2018-02-06 16:52:42 +0800

    Action mailer tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,6 +18,26 @@ describe UsersController do
       end
     end
 
+    context "Action mailer" do
+      let(:user_attributes) { attributes_for(:user) }
+      after { ActionMailer::Base.deliveries.clear }
+      it "sends out an email to the user" do
+        post :create, user: user_attributes
+        expect(ActionMailer::Base.deliveries.last.to).to eq [user_attributes[:email]]
+      end
+
+      it "sends out with user name" do
+        post :create, user: user_attributes
+        expect(ActionMailer::Base.deliveries.last.body).to include user_attributes[:full_name]
+      end
+
+      it "does not send out email with invalid input" do
+        user_attributes[:full_name] = nil
+        post :create, user: user_attributes
+        expect(ActionMailer::Base.deliveries).to be_empty
+      end
+    end
+
     context "with invalid input" do
       it "sets error message" do
         post :create, user: attributes_for(:user, email: nil)
