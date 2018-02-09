f029a3c61c2ee94cf6f1cf0f2d4a030bf84bc774
Awlter <wxfwalter@gmail.com>
2018-02-09 20:55:18 +0800

    eliminate 'delay' extention from sidekiq for mailer to test Mailgun

--- app/controllers/forget_passwords_controller.rb
+++ app/controllers/forget_passwords_controller.rb
@@ -3,7 +3,7 @@ class ForgetPasswordsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user
       user.add_reset_token
-      MyMailer.delay.forget_password(user)
+      MyMailer.forget_password(user)
     end
 
     redirect_to root_path
